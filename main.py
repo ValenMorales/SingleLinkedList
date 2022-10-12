f13d5afa850618e2438bf29b49aa8306eaa0b141
valen <valentina.moraless@autonoma.edu.co>
2022-10-12 10:20:04 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from singleLinkedlist import SingleLinkedList
+inst_sll=SingleLinkedList()
+inst_sll.push_node('A')
+inst_sll.unshift_node('B')
+inst_sll.insert_node(3, 'c')
+inst_sll.insert_node(2, 'd')
+
+inst_sll.show_info_sll()
